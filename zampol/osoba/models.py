4429b7e68ed1c081d70c2deacace7d35245b59ba
Vadym Rud <VadymRud@users.noreply.github.com>
2021-02-02 23:26:18 +0200

    3 commit

--- zampol/osoba/models.py
+++ zampol/osoba/models.py
@@ -1,15 +1,23 @@
 from django.db import models
 
+
 class MilitaryRanks(models.Model):
+    name = models.CharField(max_length=512)
+
+
+class Platoon(models.Model):
+    name = models.CharField(max_length=512)
+
+
+class Unit(models.Model):
     name = models.CharField(max_length=512)
 
 
 class ServiseID(models.Model):
     name = models.CharField(max_length=512)
     sename = models.CharField(max_length=512)
-    thirdName = models.CharField(max_length=512)
-    birthDate = models.DateField()
-
-
-
-
+    third_name = models.CharField(max_length=512)
+    birth_date = models.DateField()
+    military_ranks = models.ForeignKey(MilitaryRanks, on_delete=models.SET_NULL)
+    unit = models.ForeignKey(Unit, on_delete=models.SET_NULL)
+    platoon = models.ForeignKey(Platoon, on_delete=models.SET_NULL)
---
+++ zampol/pidrozdil/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PidrozdilConfig(AppConfig):
+    name = 'pidrozdil'
