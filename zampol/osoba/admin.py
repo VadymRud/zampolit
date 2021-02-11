1015b65664490be7763b85808671067f9b661452
VadymRud <rudvadim@gmail.com>
2021-02-11 15:33:15 +0300

    добавив статік

--- zampol/osoba/admin.py
+++ zampol/osoba/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import MilitaryRank, Platoon, ServiseID, Unit
+admin.site.register(MilitaryRank)
+admin.site.register(Platoon)
+admin.site.register(ServiseID)
+admin.site.register(Unit)
--- zampol/osoba/models.py
+++ zampol/osoba/models.py
@@ -1,16 +1,25 @@
 from django.db import models
 
 
-class MilitaryRanks(models.Model):
+class MilitaryRank(models.Model):
     name = models.CharField(max_length=512)
+    
+    def __str__(self):
+        return self.name[:50]
 
 
 class Platoon(models.Model):
     name = models.CharField(max_length=512)
+    
+    def __str__(self):
+        return self.name[:50]
 
 
 class Unit(models.Model):
     name = models.CharField(max_length=512)
+    
+    def __str__(self):
+        return self.name[:50]
 
 
 class ServiseID(models.Model):
@@ -18,6 +27,9 @@ class ServiseID(models.Model):
     sename = models.CharField(max_length=512)
     third_name = models.CharField(max_length=512)
     birth_date = models.DateField()
-    military_ranks = models.ForeignKey(MilitaryRanks, on_delete=models.CASCADE)
+    military_ranks = models.ForeignKey(MilitaryRank, on_delete=models.CASCADE)
     unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
     platoon = models.ForeignKey(Platoon, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return '{} {} {} {} {}'.format(self.unit.name, self.military_ranks.name, self.name ,self.sename, self.third_name)
