9cf0c54b7aae0ce258a7f57d7e5ed6cd7acc8d56
Vadym Rud <VadymRud@users.noreply.github.com>
2021-06-04 12:28:31 +0300

    asdfasdfafasdf

--- zampol/relocation/admin.py
+++ zampol/relocation/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.utils.translation import gettext as _
+from .models import Status, Relocation
 
 # Register your models here.
+admin.site.register(Relocation)
+admin.site.register(Status)
--- zampol/relocation/models.py
+++ zampol/relocation/models.py
@@ -5,6 +5,16 @@ from django.utils.translation import gettext as _
 # Create your models here.
 
 
+class Status(models.Model):
+    description = models.TextField(verbose_name=_('Description'), blank=True)
+    def __str__(self):
+        return self.name[:50]
+
+    class Meta:
+        verbose_name = _('Description')
+        verbose_name_plural = _('Descriptions')
+
+
 class Relocation(models.Model):
     name = models.CharField(max_length=512, verbose_name=_('Name'), blank=True)
     from_staff = models.ForeignKey(Staff, on_delete=models.CASCADE, blank=True, verbose_name= _('from staff'),
@@ -16,6 +26,7 @@ class Relocation(models.Model):
     appruve = models.BooleanField(verbose_name=_('order number'), default=False)
     appruve_date = models.DateField(verbose_name=_('appruve date'), auto_now_add=True)
     osoba = models.ForeignKey(ServiseID, on_delete=models.CASCADE, verbose_name= _('osoba'))
+    status = models.ForeignKey(Status, on_delete=models.CASCADE, verbose_name= _('status'), null=True)
 
     def __str__(self):
         return self.name[:50]
@@ -23,3 +34,5 @@ class Relocation(models.Model):
     class Meta:
         verbose_name = _('Relocation')
         verbose_name_plural = _('Relocation\'s')
+
+
